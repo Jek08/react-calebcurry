61b974b96acb49cc0ad2e1715fe88b9dd19911a8
Jek08 <jk.pratama86@gmail.com>
2023-11-21 00:43:23 +0700

    add employee cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,47 @@ function App() {
     const [role, setRole] = useState("dev");
 
     return (
-        <>
-            <input
+        <div className="App">
+            {/* <input
                 type="text"
                 onChange={(e) => {
                     e.preventDefault();
                     setRole(e.target.value);
                 }}
-            />
-            <Employee name="Nuna" role={role} />
-            <Employee name="Emily" role={role} />
-        </>
+            /> */}
+            <div className="flex flex-wrap">
+                <Employee
+                    name="Nuna"
+                    role={role}
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTTQuyO8Mw-k4JUoKh5MnfhfMz4AycLb2Ujw&usqp=CAU"
+                />
+                <Employee
+                    name="Emily"
+                    role={role}
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTTQuyO8Mw-k4JUoKh5MnfhfMz4AycLb2Ujw&usqp=CAU"
+                />
+                <Employee
+                    name="Will"
+                    role={role}
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTTQuyO8Mw-k4JUoKh5MnfhfMz4AycLb2Ujw&usqp=CAU"
+                />
+                <Employee
+                    name="Nuna"
+                    role={role}
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTTQuyO8Mw-k4JUoKh5MnfhfMz4AycLb2Ujw&usqp=CAU"
+                />
+                <Employee
+                    name="Emily"
+                    role={role}
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTTQuyO8Mw-k4JUoKh5MnfhfMz4AycLb2Ujw&usqp=CAU"
+                />
+                <Employee
+                    name="Will"
+                    role={role}
+                    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTTQuyO8Mw-k4JUoKh5MnfhfMz4AycLb2Ujw&usqp=CAU"
+                />
+            </div>
+        </div>
     );
 }
 
