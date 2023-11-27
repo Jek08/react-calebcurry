e60a7b80c87d459325639583a2e5aa87a44f8ec8
Jek08 <jk.pratama86@gmail.com>
2023-11-27 07:24:32 +0700

    how to handle error

--- src/pages/Definition.jsx
+++ src/pages/Definition.jsx
@@ -7,6 +7,7 @@ import NotFound from "./NotFound";
 export default function Definition() {
     const [word, setWord] = useState();
     const [notFound, setNotFound] = useState(false);
+    const [error, setError] = useState(false);
     const navigate = useNavigate();
     let { search } = useParams();
 
@@ -15,11 +16,23 @@ export default function Definition() {
             .then((response) => {
                 if (response.status == 404) {
                     setNotFound(true);
+                } else if (response.status == 401) {
+                    navigate("/login");
+                } else if (response.status == 500) {
+                    setError(true);
+                }
+
+                if (!response.ok) {
+                    setError(true);
+                    throw new Error("Something went wrong");
                 }
                 return response.json();
             })
             .then((data) => {
                 setWord(data[0].meanings);
+            })
+            .catch((e) => {
+                console.log(e.message);
             });
     }, []);
 
@@ -27,6 +40,14 @@ export default function Definition() {
         return (
             <>
                 <NotFound />
+                <Link to="/dictionary">Search another</Link>
+            </>
+        );
+    }
+    if (error === true) {
+        return (
+            <>
+                <p>Something went wrong</p>
                 <Link to="/dictionary">Search another</Link>
             </>
         );
