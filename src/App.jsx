12086ffc416d7e436277540b3c57dbc258b68a20
Jek08 <jk.pratama86@gmail.com>
2023-12-03 01:46:46 +0700

    auto refresh token

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Employees from "./pages/Employees";
 import Customers from "./pages/Customers";
 import Customer from "./pages/Customer";
@@ -9,6 +9,7 @@ import Dictionary from "./pages/Dictionary";
 import Definition from "./pages/Definition";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
+import { baseUrl } from "./shared";
 
 export const LoginContext = createContext();
 
@@ -16,6 +17,38 @@ function App() {
     const [loggedIn, setLoggedIn] = useState(
         localStorage.access ? true : false
     );
+
+    useEffect(() => {
+        const minute = 1000 * 60;
+
+        function refreshToken() {
+            if (localStorage.refresh) {
+                const url = baseUrl + "api/token/refresh";
+                fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        refresh: localStorage.refresh,
+                    }),
+                })
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        localStorage.access = data.access;
+                        localStorage.refresh = data.refresh;
+                        setLoggedIn(true);
+                    });
+            }
+        }
+
+        refreshToken();
+        setInterval(() => {
+            refreshToken();
+        }, minute * 3);
+    }, []);
 
     function changeLoggedIn(value) {
         setLoggedIn(false);
